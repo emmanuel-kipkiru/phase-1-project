fba23378e39dcc281cc46ccc9112f1a3b32665cd
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-06-24 02:06:19 +0300

    Done.

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('button');
+let box = document.querySelector('#box');
+btn.addEventListener('click', () => {
+    box.click();
+});
+box.addEventListener('click', (e) => {
+    box.style.backgroundColor = "green"
+})
+
